87bc799a213749c8b430939bfc695fc02e0fad05
Zane Perry <Zane.Perry@colorado.edu>
2023-11-27 20:41:57 -0700

    Givens algorithm for QR

--- QRPermutation.py
+++ QRPermutation.py
@@ -1,21 +1,27 @@
 import numpy as np
-import math
 import random
-import numpy.linalg as la
 import scipy
 import time
+from generateDeficientMatrix import generate_matrix
 
 
 def driver():
 
 
-    A = generate_matrix(10,10,8)
+    A = generate_matrix(100,100,87)
 
     [Ak, R11, R12, Q11] = QRrankKApproximation1(A)
 
+    #A = np.array([[16,15,14,13],[12,11,10,9],[8,7,6,5],[4,3,2,1]])
+    #A = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+    #A = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    #A = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]])
+
+    [Ak, R11, R12, Q11] = QRrankKApproximation2(A)
 
 
-def permutedQR1(A, k=0, tol=1.e-6):
+
+def permutedQR1(A, k=0, tol=1.e-3):
 
     start = time.time()
     Q = A.copy()
@@ -30,7 +36,8 @@ def permutedQR1(A, k=0, tol=1.e-6):
     c = np.zeros(n)
 
     for j in range(n):
-        c[j] = np.linalg.norm(Q[:,j])
+        v = Q[:,j]
+        c[j] = np.matmul(np.transpose(v),v)
 
     r = 0
 
@@ -44,7 +51,6 @@ def permutedQR1(A, k=0, tol=1.e-6):
                 max = j
                 maxNorm = norm
         if(abs(maxNorm) < tol and not forcedRank):
-            print(maxNorm)
             break
         r += 1
         if(max != i):
@@ -61,26 +67,99 @@ def permutedQR1(A, k=0, tol=1.e-6):
 
         for k in range(i + 1, n):
             xk = Q[:,k]
-            xk = xk - (np.inner(ui,xk) * ui)
+            inner = np.inner(ui,xk)
+            xk = xk - (inner * ui)
             Q[:,k] = xk
-            c[k] = np.linalg.norm(Q[i:,k])
+            v = Q[i:,k]
+            c[k] = np.matmul(np.transpose(v),v)
 
     P = np.zeros([n,n])
     for i in range(n):
         P[p[i],i] = 1
 
-    R = np.transpose(Q)@A@P
+    R = np.matmul(np.matmul(np.transpose(Q),A),P)
 
     end = time.time()
     duration = end - start
     
     return [Q,R,P,r,duration]
 
-def QRrankKApproximation1(A,k=0,tol=1.e-6):
 
 
-    [m, n] = A.shape
-    [Q,R,P,r,duration] = permutedQR1(A)
+def QRGivens(A,k=0,tol=1.e-3):
+
+
+    start = time.time()
+    R = A.copy()
+    [m,n] = R.shape
+    c = np.zeros(n)
+    Q = np.eye(m)
+    p = np.array(range(n))
+
+
+    for j in range(n):
+        v = R[:,j]
+        c[j] = np.matmul(np.transpose(v),v)
+
+    for l in range(n):
+        
+        max = l
+        maxNorm = c[l]
+        for j in range(l + 1, n):
+            norm = c[j]
+            if(norm > maxNorm):
+                max = j
+                maxNorm = norm
+        if(max != l):
+            temp = R[:,l].copy()
+            R[:,l] = R[:,max]
+            R[:,max] = temp
+            c[l], c[max] = c[max], c[l]
+            p[l], p[max] = p[max], p[l]
+        
+
+        for j in range(l+1,n):
+            c[j] = c[j] - (R[l,j]**2)
+
+        for j in range(l):
+            G = buildGivens(l,j,R[j,j],R[l,j],m)
+            R = np.matmul(G,R)
+            Q = np.matmul(Q,np.transpose(G),Q)
+
+    P = np.zeros([n,n])
+    for i in range(n):
+        P[p[i],i] = 1
+
+    end = time.time()
+
+    duration = end - start
+
+    return [Q,R,P,duration]
+    
+
+        
+
+
+
+
+def buildGivens(i,j,a,b,m):
+
+    r = np.hypot(a,b)
+    c = a / r
+    s = -b / r
+
+    G = np.eye(m)
+    G[i,i] = c
+    G[j,j] = c
+    G[i,j] = s
+    G[j,i] = -s
+
+    return G
+
+
+
+def QRrankKApproximation1(A,k=0,tol=1.e-3):
+
 
     if(k==0):
         [Q,R,P,k,duration] = permutedQR1(A)
@@ -91,65 +170,46 @@ def QRrankKApproximation1(A,k=0,tol=1.e-6):
     R12 = R[0:k, k:]
     Q11 = Q[:, 0:k]
 
-    Ak = np.block([Q11@R11, Q11@R12])
+    Ak = np.block([np.matmul(Q11,R11), np.matmul(Q11,R12)])
 
-    diff = np.linalg.norm(A - Ak@np.transpose(P))
+    diff = np.linalg.norm(A - np.matmul(Ak,np.transpose(P)))
 
     print('Time to create QR factorization 1:', duration)
-    print('Rank ', r, 'approximation')
+    print('Rank ', k, 'approximation')
     print('Error of ', diff)
 
-    #print(A)
-    #print(Q@R@np.transpose(P))
-    print(np.diag(R))
 
     return [Ak, R11, R12, Q11]
 
 
 
+def QRrankKApproximation2(A,k=0,tol=1.e-3):
+
+    [m,n] = A.shape
+
+    [Q,R,P,duration] = QRGivens(A,k=k,tol=tol)
+
+    if(k==0):
+        diagonals = np.diag(R)
+        for i in range(min([m,n])):
+            if(diagonals[i] < tol):
+                break
+            k += 1
+
+    R11 = R[0:k,0:k]
+    R12 = R[0:k, k:]
+    Q11 = Q[:, 0:k]
+
+    Ak = np.block([np.matmul(Q11,R11), np.matmul(Q11,R12)])
+
+    diff = np.linalg.norm(A - np.matmul(Ak,np.transpose(P)))
+
+    print('Time to create QR factorization 2:', duration)
+    print('Rank ', k, 'approximation')
+    print('Error of ', diff)
 
 
-def rand_sing(dim, rank, tol=1.e-6):
-    # randomly generate rank singular values that are larger than the tol
-    
-    large_sing = np.zeros(rank)
-    small_sing = np.zeros(dim - rank)
-    
-    for i in range(rank):
-        large_sing[i] = random.uniform(10, 420)
-        
-    for j in range(dim - rank):
-        small_sing[j] = random.uniform(0, tol)
-        
-    diag = np.append(large_sing, small_sing)
-
-    np.random.shuffle(diag)
-
-    
-    diag_matrix = np.diag(diag)
-    
-    return diag_matrix
-
-
-def generate_matrix(m,n,r):
-
-    U = scipy.stats.ortho_group.rvs(m)
-    V = scipy.stats.ortho_group.rvs(n)
-
-    if(m < n):
-        Sigma = rand_sing(m,r)
-        block = np.zeros([m, n-m])
-        Sigma = np.block([Sigma, block])
-    elif(m > n):
-        Sigma = rand_sing(n,r)
-        block = np.zeros([m-n,n])
-        Sigma = np.block([[Sigma],[block]])
-    else:
-        Sigma = rand_sing(m,r)
-
-    return U@Sigma@V
-
-
+    return [Ak, R11, R12, Q11]
 
 
 
