194dd01a6e58f289dae0e4f16fc391b512cf371b
sleeklymauler <luke.sell@icloud.com>
2023-12-01 18:29:08 -0700

    Started work on removing white noise from audio file in testAudio.py

--- testAudio.py
+++ testAudio.py
@@ -1,17 +1,63 @@
 import os
+import scipy as sp
 
 from helpers import recordAudioToDataVector, dataVectorToWavFile,\
-    wavFileToDataVector
+    wavFileToDataVector, SVDrankKApproximation
+
+
+## - get data vector of maedee_voice.wav and white_noise.wav, add them together, 
+##   and create .wav file from it
+##
+
+currentDirectory = os.path.dirname(__file__)
+
+fileToImport = "maedee_voice.wav"
+fileLocation = os.path.join(currentDirectory, "Audio/", fileToImport)
+(maedeeSampleRate, maedataVector) = wavFileToDataVector(fileLocation)
+
+fileToImport = "white_noise.wav"
+fileLocation = os.path.join(currentDirectory, "Audio/", fileToImport)
+(whiteNoiseSampleRate, whiteNoiseDataVector) = wavFileToDataVector(fileLocation)
+
+combinedDataVector = maedataVector + whiteNoiseDataVector
+dataVectorToWavFile(combinedDataVector, maedeeSampleRate, "combined")
+
+## (try to) remove the white noise from the combined data vector
+##
+
+# - split data vector into 30 millisecond windows
+#   (maedeeSampleRate [samples] / 1 [s]) * (5 [s]) = 
+#   5 * maedeeSampleRate [samples]
+totalSamples = maedeeSampleRate * 5
+# (5 [s]) / ( (30 [ms] / 1 [window]) * (1 [s] / 1000 [ms]) ) = 5/0.03 [windows]
+numWindows = 5 / 0.03
+# (totalSamples [samples]) / numWindows [windows] = [samples / window]
+samplesPerWindow = totalSamples / numWindows
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## convert .wav file to a representative data vector
 ##
 
-currentDirectory = os.path.dirname(__file__)
-fileToImport = "maedee_voice.wav"
-fileLocation = os.path.join(currentDirectory, "Audio/", fileToImport)
+# currentDirectory = os.path.dirname(__file__)
+# fileToImport = "maedee_voice.wav"
+# fileLocation = os.path.join(currentDirectory, "Audio/", fileToImport)
 
-sampleRate, dataVector = wavFileToDataVector(fileLocation)
-print(dataVector)
+# sampleRate, dataVector = wavFileToDataVector(fileLocation)
+# print(dataVector)
 
 ## debugging 
 ##
