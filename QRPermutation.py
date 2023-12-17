49332882479de1f2944c22606bd1ecb1cf96c724
Zane Perry <Zane.Perry@colorado.edu>
2023-12-17 16:28:20 -0700

    Fixed QR algos so they work with all dimensions

--- QRPermutation.py
+++ QRPermutation.py
@@ -8,7 +8,12 @@ from generateDeficientMatrix import generate_matrix
 def driver():
 
 
-    A = generate_matrix(100,100,43)
+    m = random.randint(4,100)
+    n = random.randint(4,100)
+    minDim = min([m,n])
+    r = minDim - random.randint(1,minDim - 3)
+
+    A = generate_matrix(m,n,r)
 
     #A = np.array([[16,15,14,13],[12,11,10,9],[8,7,6,5],[4,3,2,1]])
     #A = np.array([[1.,2.,3.,4.],[5.,6.,7.,8.],[9.,10.,11.,12.],[13.,14.,15.,16.]])
@@ -18,9 +23,20 @@ def driver():
 
 
 
-    #[k, P, Q, R, Ak, Q11, R11, R12, duration] = msgQRrankKApproximation(A)
-    #[k, P, Q, R, Ak, Q11, R11, R12, duration] = householderQRrankKApproximation(A)
-    #[k, P, Q, R, Ak, Q11, R11, R12, duration] = givensQRrankKApproximation(A)
+    print('QR factorizations for an', m, 'by', n, 'matrix with numerical rank', r)
+    [k, P, Q, R, Ak, Q11, R11, R12, duration, diff] = msgQRrankKApproximation(A)
+    print('Time to create QR factorization (Modified Gram-Schmidt):', duration)
+    print('Rank ', k, 'approximation')
+    print('Error of ', diff)
+    [k, P, Q, R, Ak, Q11, R11, R12, duration, diff] = householderQRrankKApproximation(A)
+    print('Time to create QR factorization (Householder Reflections):', duration)
+    print('Rank ', k, 'approximation')
+    print('Error of ', diff)
+    [k, P, Q, R, Ak, Q11, R11, R12, duration, diff] = givensQRrankKApproximation(A)
+    print('Time to create QR factorization (Givens Rotations):', duration)
+    print('Rank ', k, 'approximation')
+    print('Error of ', diff)
+
     
 
 
@@ -41,12 +57,7 @@ def msgQRrankKApproximation(A,k=0,tol=1.e-3):
 
     diff = np.linalg.norm(A - Ak)
 
-    print('Time to create QR factorization (Modified Gram-Schmidt):', duration)
-    print('Rank ', k, 'approximation')
-    print('Error of ', diff)
-
-
-    return [k, P, Q, R, Ak, Q11, R11, R12, duration]
+    return [k, P, Q, R, Ak, Q11, R11, R12, duration, diff]
 
 
 
@@ -65,12 +76,8 @@ def householderQRrankKApproximation(A,k=0,tol=1.e-3):
 
     diff = np.linalg.norm(A - np.matmul(Ak,np.transpose(P)))
 
-    print('Time to create QR factorization (Householder Reflections):', duration)
-    print('Rank ', k, 'approximation')
-    print('Error of ', diff)
 
-
-    return [k, P, Q, R, Ak, Q11, R11, R12, duration]
+    return [k, P, Q, R, Ak, Q11, R11, R12, duration, diff]
 
 
 
@@ -89,12 +96,8 @@ def givensQRrankKApproximation(A,k=0,tol=1.e-3):
 
     diff = np.linalg.norm(A - np.matmul(Ak,np.transpose(P)))
 
-    print('Time to create QR factorization (Givens Rotations):', duration)
-    print('Rank ', k, 'approximation')
-    print('Error of ', diff)
 
-
-    return [k, P, Q, R, Ak, Q11, R11, R12, duration]
+    return [k, P, Q, R, Ak, Q11, R11, R12, duration, diff]
 
 
 
@@ -155,7 +158,7 @@ def mgsQR(A, k=0, tol=1.e-3):
             c[k] = np.matmul(np.transpose(v),v)
 
     if(not foundRank):
-        r = n
+        r = min([m,n])
 
     P = np.zeros([n,n])
     for i in range(n):
@@ -175,7 +178,7 @@ def householderQR(A, k=0, tol=1.e-3):
     start = time.time()
     R = A.copy()
     [m,n] = R.shape
-    Q = np.eye(n)
+    Q = np.eye(m)
     forcedRank = True
 
     if(k == 0):
@@ -194,8 +197,7 @@ def householderQR(A, k=0, tol=1.e-3):
 
     r = 0
 
-    for i in range(k-1):
-
+    for i in range(k):
 
         max = i
         maxNorm = c[i]
@@ -206,6 +208,8 @@ def householderQR(A, k=0, tol=1.e-3):
                 maxNorm = norm
         if(abs(maxNorm) < tol and not forcedRank):
             foundRank = True
+            break
+        if(i == m - 1):
             break
         r += 1
         if(max != i):
@@ -219,7 +223,7 @@ def householderQR(A, k=0, tol=1.e-3):
         v = R[i:,i].copy()
         norm = np.linalg.norm(v,ord=2)
         v[0] = v[0] - norm
-        H = buildHouseholder(v,n,i)
+        H = buildHouseholder(v,m,i)
         R = H@R
         Q = Q@H
 
@@ -228,7 +232,7 @@ def householderQR(A, k=0, tol=1.e-3):
             c[k] = np.matmul(np.transpose(v),v)
 
     if(not foundRank):
-        r = n
+        r = min([m,n])
     P = np.zeros([n,n])
     for i in range(n):
         P[p[i],i] = 1
@@ -239,15 +243,14 @@ def householderQR(A, k=0, tol=1.e-3):
     return [Q,R,P,r,duration]
 
 
-def buildHouseholder(v,n,i):
+def buildHouseholder(v,m,i):
 
     P = 2 * np.outer(v,np.transpose(v)) / (np.transpose(v)@v)
-    Hi = np.eye(n-i) - P
+    Hi = np.eye(m-i) - P
     if(i == 0):
         return Hi
     else:
-        H = np.block([[np.eye(i), np.zeros([i,n-i])],[np.zeros([n-i,i]), Hi]])
-        return H
+        return np.block([[np.eye(i), np.zeros([i,m-i])],[np.zeros([m-i,i]), Hi]])
 
 
 
@@ -275,7 +278,7 @@ def givensQR(A,k=0,tol=1.e-3):
 
     r = 0
 
-    for i in range(k-1):
+    for i in range(k):
         
         max = i
         maxNorm = c[i]
@@ -293,28 +296,24 @@ def givensQR(A,k=0,tol=1.e-3):
         if(abs(maxNorm) < tol and not forcedRank):
             foundRank = True
             break
+        if(i == m - 1):
+            break
         r += 1
 
         
 
-        for j in range(i + 1, n):
-            G = buildGivens(j,i,R[i,i],R[j,i],n)
+        for j in range(i + 1, m):
+            G = buildGivens(j,i,R[i,i],R[j,i],m)
             R = np.matmul(G,R)
             Q = np.matmul(Q,np.transpose(G),Q)
 
         for k in range(i+1,n):
             v = R[i+1:,k]
             c[k] = np.matmul(np.transpose(v),v)
-        '''
-        for j in range(i):
-            G = buildGivens(i,j,R[j,j],R[i,j],m)
-            R = np.matmul(G,R)
-            Q = np.matmul(Q,np.transpose(G),Q)
-        '''
         
 
     if(not foundRank):
-        r = n
+        r = min([m,n])
 
     P = np.zeros([n,n])
     for i in range(n):
